0e02c565a6d0b628bc2ffb4b3d4e77c0aa2548de
Zeng Ke <superisaac.ke@gmail.com>
2021-12-27 17:40:39 +0800

    fix balancer test

--- balancer/balancer_test.go
+++ balancer/balancer_test.go
@@ -19,9 +19,14 @@ func TestBalancer(t *testing.T) {
 	b := NewBalancer()
 
 	chain := ChainRef{Name: "binance-chain", Network: "mainnet"}
-	ep := &Endpoint{Name: "binance-chain", Chain: chain, ServerUrl: "http://127.0.0.1:5432"}
+	ep := &Endpoint{Name: "binance-chain", Chain: chain, ServerUrl: "http://127.0.0.1:5432", Healthy: true}
 
 	b.Add(ep)
 
 	assert.Equal(1, len(b.nameIndex))
+	assert.Equal(1, len(b.chainIndex))
+
+	ep1, ok := b.Select(chain, -1, "")
+	assert.True(ok)
+	assert.Equal(ep.ServerUrl, ep1.ServerUrl)
 }
