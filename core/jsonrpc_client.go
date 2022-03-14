42e705e0038ca63085b40c9bf81446fb9a3982d3
Zeng Ke <superisaac.ke@gmail.com>
2022-03-14 17:34:11 +0800

    connectRPC to ensureRPCClient

--- core/jsonrpc_client.go
+++ core/jsonrpc_client.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func (self *Endpoint) connectRPC() {
+func (self *Endpoint) ensureRPCClient() {
 	if self.rpcClient == nil {
 		c, err := jsonzhttp.NewClient(self.Config.Url)
 		if err != nil {
@@ -19,19 +19,19 @@ func (self *Endpoint) connectRPC() {
 }
 
 func (self *Endpoint) RPCClient() jsonzhttp.Client {
-	self.connectRPC()
+	self.ensureRPCClient()
 	return self.rpcClient
 }
 
 func (self *Endpoint) CallRPC(rootCtx context.Context, reqmsg *jsonz.RequestMessage) (jsonz.Message, error) {
 	//self.Connect()
-	self.connectRPC()
+	self.ensureRPCClient()
 	return self.rpcClient.Call(rootCtx, reqmsg)
 } // CallRPC
 
 func (self *Endpoint) UnwrapCallRPC(rootCtx context.Context, reqmsg *jsonz.RequestMessage, output interface{}) error {
 	//self.Connect()
-	self.connectRPC()
+	self.ensureRPCClient()
 	return self.rpcClient.UnwrapCall(rootCtx, reqmsg, output)
 } // UnwrapCallRPC
 
