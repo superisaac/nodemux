280d0a45428b8afe67cb5e46f53cf8e1595fe588
Zeng Ke <superisaac.ke@gmail.com>
2023-10-07 16:35:50 +0800

    v0.2.1, retrieve height info from get_block request

--- nodemux/chains/eos.go
+++ nodemux/chains/eos.go
@@ -1,14 +1,21 @@
 package chains
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"github.com/superisaac/nodemux/core"
+	"io"
 	"net/http"
 )
 
 type eosChainInfo struct {
 	LastBlockNum int    `json:"last_irreversible_block_num"`
 	LastBlockId  string `json:"last_irreversible_block_id"`
+}
+
+type eosChainGetBlockReq struct {
+	BlockNumOrId int `json:"block_num_or_id"`
 }
 
 type EosChain struct {
@@ -44,5 +51,20 @@ func (self *EosChain) GetBlockhead(context context.Context, b *nodemuxcore.Multi
 
 func (self *EosChain) DelegateREST(rootCtx context.Context, b *nodemuxcore.Multiplexer, chain nodemuxcore.ChainRef, path string, w http.ResponseWriter, r *http.Request) error {
 	// Custom relay methods can be defined here
-	return b.DefaultPipeREST(rootCtx, chain, path, w, r, -30)
+	h := -10
+	if path == "/v1/chain/get_block" {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			return err
+		}
+		getBlockReq := eosChainGetBlockReq{}
+		if err := json.Unmarshal(body, &getBlockReq); err == nil {
+			h = getBlockReq.BlockNumOrId
+			chain.Log().Infof("retrieved block number %d from get_block request", h)
+		}
+		r.Body = io.NopCloser(bytes.NewBuffer(body))
+	} else if path == "/v1/chain/get_info" {
+		h = 0
+	}
+	return b.DefaultPipeREST(rootCtx, chain, path, w, r, h)
 }
