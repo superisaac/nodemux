284362c626a71ad31f5011f433d92586fbaf59ac
Zeng Ke <superisaac.ke@gmail.com>
2024-09-18 11:20:49 +0800

    v0.4.3; custom headers in dail

--- cmds/dail/main.go
+++ cmds/dail/main.go
@@ -7,13 +7,32 @@ import (
 	"github.com/superisaac/nodemux/chains"
 	"github.com/superisaac/nodemux/core"
 	"os"
+	"strings"
 	"time"
 )
+
+func parseHeaders(line string) map[string]string {
+	if line == "" {
+		return nil
+	}
+	headers := make(map[string]string)
+	for _, h := range strings.Split(line, ";") {
+		h = strings.Trim(h, " \t")
+		kv := strings.SplitN(h, ": ", 2)
+		if len(kv) >= 2 {
+			headers[strings.Trim(kv[0], " \t")] = strings.Trim(kv[1], " \t")
+		} else if len(kv) == 1 {
+			headers[strings.Trim(kv[0], " \t")] = ""
+		}
+	}
+	return headers
+}
 
 func main() {
 	dailFlags := flag.NewFlagSet("nodemux-dail", flag.ExitOnError)
 	pChain := dailFlags.String("chain", "", "the chain namespace/network")
 	pUrl := dailFlags.String("url", "", "the rpc url to connect")
+	pHeadersLine := dailFlags.String("headers", "", "http headers")
 
 	dailFlags.Parse(os.Args[1:])
 	factory := nodemuxcore.GetDelegatorFactory()
@@ -34,9 +53,12 @@ func main() {
 		os.Exit(1)
 	}
 
+	headers := parseHeaders(*pHeadersLine)
+
 	epcfg := nodemuxcore.EndpointConfig{
-		Chain: chainref.String(),
-		Url:   *pUrl,
+		Chain:   chainref.String(),
+		Url:     *pUrl,
+		Headers: headers,
 	}
 	endpoint := nodemuxcore.NewEndpoint("endpoint001", epcfg)
 	multiplexer := nodemuxcore.NewMultiplexer()
@@ -45,7 +67,7 @@ func main() {
 	delegator := factory.GetBlockheadDelegator(endpoint.Chain.Namespace)
 	start := time.Now()
 	block, err := delegator.GetBlockhead(context.Background(), multiplexer, endpoint)
-	delta := time.Now().Sub(start)
+	delta := time.Since(start) //time.Now().Sub(start)
 	if err != nil {
 		panic(err)
 	}
